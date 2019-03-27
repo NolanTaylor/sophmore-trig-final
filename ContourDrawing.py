5e1cdd5aff0c47e00a2aa637e31c2cf17af58e65
Nolan Taylor <ItsaPotato2@gmail.com>
2019-03-27 10:29:09 -0700

    drawing lines (not very well)

--- ContourDrawing.py
+++ ContourDrawing.py
@@ -1,7 +1,10 @@
 import numpy
 import cv2
+import serial
 
 cap = cv2.VideoCapture(0)
+
+#ser = serial.Serial('COM9', 9600)
 
 lower = numpy.array([29, 86, 6])
 upper = numpy.array([64, 255, 255])
@@ -9,7 +12,7 @@ upper = numpy.array([64, 255, 255])
 while(True):
     ret, frame = cap.read()
 
-    blur = cv2.blur(frame, (20, 20))
+    blur = cv2.GaussianBlur(frame, (25, 25), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
     canny = cv2.Canny(frame, 100, 150)
 
@@ -23,25 +26,21 @@ while(True):
     res = cv2.bitwise_and(frame, frame, mask = mask)
 
     if len(contours) != 0:
-        leftmost = tuple(contours[0][contours[0][:,:,0].argmin()][0])
-        rightmost = tuple(contours[0][contours[0][:,:,0].argmax()][0])
-        topmost = tuple(contours[0][contours[0][:,:,1].argmin()][0])
-        bottommost = tuple(contours[0][contours[0][:,:,1].argmax()][0])
-
-        cv2.circle(frame, leftmost, 5, (255, 10, 10), -1)
-        cv2.circle(frame, rightmost, 5, (255, 10, 10), -1)
-        cv2.circle(frame, topmost, 5, (255, 10, 10), -1)
-        cv2.circle(frame, bottommost, 5, (255, 10, 10), -1)
-
         c = max(contours, key = cv2.contourArea)
 
         x,y,w,h = cv2.boundingRect(c)
 
-        cv2.rectangle(res, (x,y), (x+w,y+h), (255,0,0), 2)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+
+        cv2.line(frame, (x, y + (h - y)/2), (w, y + (h - y)/2), (10, 10, 255), 2)
+        cv2.line(frame, (x + (w - x)/2, y), (x + (w - x)/2, h), (10, 10, 255), 2)
+
+        cv2.line(frame, (x, y), (x + w, y + h), (10, 10, 255), 2)
 
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
     cv2.imshow('canny', canny)
+    cv2.imshow('hsv', hsv)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
